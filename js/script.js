8660ee2a8fcbfa4687700fc86a6a5c0bee1b7b08
Preston <prestonosoro56@gmail.com>
2023-01-18 20:01:21 +0300

    refactored script.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ function opentab(tabname) {
   for (tabcontent of tabcontents) {
     tabcontent.classList.remove("active-tab");
   }
-  e.currentTarget.classList.add("active-link");
+  event.currentTarget.classList.add("active-link");
   document.getElementById(tabname).classList.add("active-tab");
 }
 
